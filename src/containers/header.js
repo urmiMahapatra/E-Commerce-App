7eeb07b2d279f096757cefd5091d2327f363c8fd
Urmi Mahapatra <urmi.mahapatra@gmail.com>
2023-03-12 01:53:08 +0530

    First commit

---
+++ src/containers/header.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const Header =() =>{
+    return (
+        <div className="ui fixed menu">
+            <div className='ui container center'>
+                <h2>SHOPPING STOP</h2>
+            </div>
+        </div>
+
+    )
+};
+
+export default Header;
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,23 @@
+import {ActionTypes} from '../constants/action-types';
+
+const initialState={
+    products :[
+        {
+            id:1,
+            title:"clutch",
+            category:"bag",
+        },
+    ],
+
+};
+
+export const productReducer =(state= initialState,{type,payload}) =>{
+    switch (type){
+        case ActionTypes.SET_PRODUCTS:
+            return state
+        default:
+            return state;    
+
+    }
+
+};
